c140813ae98842df56cc74a5c3afb0858aba51fd
Luke <lukeannison@gmail.com>
2022-10-21 21:53:52 +0100

    updated extension

--- apps/extension/src/index.js
+++ apps/extension/src/index.js
@@ -1,16 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./popup/popup";
-import { PageProvider , ToggleProvider} from "./contexts/pageContext";
-import { ProviderProvider } from "./contexts/providerContext";
 
 const app = document.getElementById("app");
 const root = createRoot(app);
 root.render(
-  <ProviderProvider>
-  <ToggleProvider>
-  <PageProvider>
     <App />
-  </PageProvider>
-  </ToggleProvider>
-    </ProviderProvider>
 );
--- apps/extension/src/popup/popup.tsx
+++ apps/extension/src/popup/popup.tsx
@@ -1,14 +1,61 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Button, Input  } from '@mui/material';
+import {QuantList} from 'ui';
 
 import { Header} from 'ui' 
 
+const quants = [
+  {
+    id: 1,
+    name: "Quant 1",
+    description: "This is the first quant",
+    created: "2021-10-01",
+    date: "2021-10-01",
+    repeat: false
+  }
+];
+
 export function App() {
+
+	const [input , setInput] = useState('');
+  const [displayQuants, setDisplayQuants] = useState(quants);
+
+  console.log({displayQuants})
+
+
+	const createQuant = () => {
+
+    
+
+      axios.post('/api/quant', {name: input}) 
+        .then(
+          (response) => {
+            setInput( "");
+            const newQuant = response.data.data;
+            console.log({newQuant});
+            setDisplayQuants([...displayQuants, newQuant]);
+            console.log('response in createQuant', response);
+          },
+          (err) => {
+            console.log(err.text);
+          }
+        );
+
+	}
+
+
+
 
   return (
     <>
-      <Header />
-    </>
+    <Header/>
+    <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
+		<Button onClick={createQuant}>New item</Button>
+		<Input value={input} onChange={e => setInput(e.target.value)}/>	
+    <QuantList quants={displayQuants} displayQuants={displayQuants} setDisplayQuants={setDisplayQuants}/>
+    </div>
+  </>
   );
 }
 
