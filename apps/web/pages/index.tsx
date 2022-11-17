e595cef14f59cb3269dfb249e33d6b1a28900242
Luke <lukeannison@gmail.com>
2022-11-17 00:53:01 +0000

    fixed rollover time and updating bug without children

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -23,9 +23,11 @@ export async function getServerSideProps() {
   
   
 await dbConnect();
-  let cutoff = new Date();
-  cutoff.setDate(cutoff.getDate());
+  // cutoff 3am today
+  const cutoff = new Date();
+console.log('cutoff', cutoff);
   // find quants that are not completed and are due before today and don't have status of 0
+  console.log('cutoff', new Date(cutoff.setHours(3,0,0,0)));
   const data = await Quant.find({status: {$ne: 0}, date: {$lte: cutoff}}).sort({date: 1}).lean();
 
   const quants = JSON.parse(JSON.stringify(data));
