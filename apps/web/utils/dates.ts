691cc865e06db7c894bfc04f3c4dc1f2c86599b4
Luke <lukeannison@gmail.com>
2022-11-26 17:49:54 +0000

    added monthly

--- apps/web/utils/dates.ts
+++ apps/web/utils/dates.ts
@@ -18,6 +18,9 @@ export const getDateFromPeriod = (period: string, date: Date) => {
 		case 'Fortnightly':
 			return format(addDays(new Date(date.setHours(3,0,0,0)), 14), 'yyyy-MM-dd hh:mm:ss');
 			break;
+		case 'Monthly':
+			return format(addDays(new Date(date.setHours(3,0,0,0)), 30), 'yyyy-MM-dd hh:mm:ss');
+			break;
 	}
 }
 
